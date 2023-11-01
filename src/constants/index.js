2bac136f22214b391dac5b89550099e1f391f464
octocatblain <blainmasaku2@gmail.com>
2023-11-01 15:32:50 +0300

    feat: add footer section

--- src/constants/index.js
+++ src/constants/index.js
@@ -134,7 +134,7 @@ export const footerLinks = [
     title: "Get in touch",
     links: [
       { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-      { name: "+92554862354", link: "tel:+92554862354" },
+      { name: "+254113604268", link: "tel:+254113604268" },
     ],
   },
 ];
