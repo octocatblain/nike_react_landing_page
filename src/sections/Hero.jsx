6d2e4f22877ba00e285e631430a0776e509d90f5
octocatblain <blainmasaku2@gmail.com>
2023-11-01 15:41:46 +0300

    fix: adjust font-size

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">
+        <p className="text-xl font-montserrat text-coral-red fontfont-bold">
           Our Summer Collection
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
